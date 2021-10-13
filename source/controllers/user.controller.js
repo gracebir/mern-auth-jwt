e23e2524e9318a3429bc7ab1bfdc735e17de6ac6
graceDev1 <grace.devolop@gmail.com>
2021-10-13 14:00:04 +0200

    writing the user registration

--- source/controllers/user.controller.js
+++ source/controllers/user.controller.js
@@ -1,5 +1,21 @@
+const UserModel = require('../models/user.model');
 
-exports.register = (req,res, next) =>{
+
+exports.register = async (req,res, next) =>{
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await UserModel.create({
+            username, email, password
+        })
+
+        res.status(201).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        res.status(501).json({error:error.message()})
+    }
     res.send('this is register');
 }
 
--- source/models/user.model.js
+++ source/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const UserSchema = new mongoose.Schema({
     username:{
@@ -20,6 +22,15 @@ const UserSchema = new mongoose.Schema({
     resetPassword: String,
     resetPasswordExpire : Date
 });
+
+
+UserSchema.pre('save', async function(next){
+    if(this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 
 module.exports = mongoose.model('user',UserSchema);
