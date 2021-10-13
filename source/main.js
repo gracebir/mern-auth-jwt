092422e86fada7382ec054d9b3d8b53a8975baa4
graceDev1 <grace.devolop@gmail.com>
2021-10-13 20:47:11 +0200

    adding the error handler middleware

--- source/main.js
+++ source/main.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const connectDb = require('./config/db');
+const errorhandler = require('./middleware/error');
+
 
 (async()=>{
     connectDb();
     const app = express()
+    app.use(express.json())
     const port = process.env.PORT || 5000;
     
     app.use('/api/auth', require('./routes/user.route'))
+    app.use(errorhandler)
     let server = app.listen(port, ()=>{
         console.log(`server running on http://localhost:${port}`);
     });
