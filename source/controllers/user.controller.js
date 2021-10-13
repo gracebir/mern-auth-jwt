f2f942d636cb3c7223692b77b2758fef440d931f
graceDev1 <grace.devolop@gmail.com>
2021-10-13 19:53:58 +0200

    implementing te login function and generate the token

--- source/controllers/user.controller.js
+++ source/controllers/user.controller.js
@@ -1,4 +1,7 @@
 const UserModel = require('../models/user.model');
+const {sign} = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 
 exports.register = async (req,res, next) =>{
@@ -14,13 +17,45 @@ exports.register = async (req,res, next) =>{
             user
         })
     } catch (error) {
-        res.status(501).json({error:error.message()})
+        res.status(501).json({error:error.message})
     }
     res.send('this is register');
 }
 
-exports.login = (req, res, next) =>{
-    res.send('this is login!!!!!!!');
+
+
+
+
+exports.login = async (req, res, next) =>{
+    const { email, password } = req.body;
+    if(!email || !password) {
+        res.status(400).json({success: false, error: "Please provide email and password"});
+    }
+
+    try {
+        const user = await UserModel.findOne({ email }).select("+password");
+        if(!user){
+            res.status(404).json({ success: false, error: "Invalid credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            res.status(404).json({ success: false, error:"Invalid Password"})
+        }
+
+
+        res.status(201).json({
+            success: true,
+            accessToken: sign(
+                {id: user._id},
+                "mern_secret",
+                {expiresIn: "15d"}, 
+            )
+        })
+    } catch (error) {
+        res.status(501).json({error: error.message})
+    }
 }
 
 
