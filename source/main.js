5fd88888b3c14708623a3e0721fb7fe40f9c373d
graceDev1 <grace.devolop@gmail.com>
2021-10-13 14:57:01 +0200

    handle error rejection

--- source/main.js
+++ source/main.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express');
-
+const connectDb = require('./config/db');
 
 (async()=>{
+    connectDb();
     const app = express()
     const port = process.env.PORT || 5000;
+    
     app.use('/api/auth', require('./routes/user.route'))
-    app.listen(port, ()=>{
+    let server = app.listen(port, ()=>{
         console.log(`server running on http://localhost:${port}`);
+    });
+    process.on("unhandledRejection",(error, promise)=>{
+        console.log("Looger error", error);
+        server.close(()=> process.exit(1));
     })
 })()
