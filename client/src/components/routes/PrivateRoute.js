f76aeaeab90fcde979b404da5bf2fad5f578930a
graceDev1 <grace.devolop@gmail.com>
2021-10-15 20:31:23 +0200

    create the private for blocking user to not access to route if is not logged in

---
+++ client/src/components/routes/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom';
+
+function PrivateRoute({component:Component, ...rest}) {
+    return (
+        <Route
+        {...rest} 
+        render={
+            (props)=>
+                localStorage.getItem("authToken") ? (
+                    <Component {...props}/>
+                ): (
+                    <Redirect to="/login" />
+                )
+            }
+        
+        />
+       
+ 
+    )
+}
+
+export default PrivateRoute
